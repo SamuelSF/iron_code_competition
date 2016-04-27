b469b5e344c2ba83046de88dfcfe64eae25bb7f0
Samuel Frade <SamuelSFrade@gmail.com>
2016-04-27 19:30:58 -0400

    sum of multiples

---
+++ sum_of_multiples.rb
@@ -0,0 +1,13 @@
+class MultipleAdder
+
+    def sum_of_multiples(n)
+        nums = []
+        (1..n).each do |i|
+            if i % 3 == 0 && i % 5 == 0
+                nums << i
+            end
+        end
+        result = nums.reduce(1) {|a, b| a * b}
+        result
+    end
+end
